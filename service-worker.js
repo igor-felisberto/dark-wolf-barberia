e53e60fee59f370dd50c898d0afe9240fbf23e26
igor-felisberto <iggoor2012@hotmail.com>
2026-09-15 13:29:21 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,37 @@
-const CACHE_NAME = "dark-wolf-v1";
+const CACHE_NAME = "dark-wolf-v2";
 
 const arquivos = [
   "./",
   "./index.html",
   "./style.css",
   "./script.js",
-  "./logo-darkwolf.png"
+  "./manifest.json",
+  "./logo-darkwolf.png",
+  "./icone-darkwolf.png"
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(arquivos);
     })
   );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((nomes) => {
+      return Promise.all(
+        nomes
+          .filter((nome) => nome !== CACHE_NAME)
+          .map((nome) => caches.delete(nome))
+      );
+    })
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
